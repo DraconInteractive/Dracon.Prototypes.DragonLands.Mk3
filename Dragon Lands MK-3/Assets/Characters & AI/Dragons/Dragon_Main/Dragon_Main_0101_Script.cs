901686f5e781eb6fd3a5ceb3a11668e22a4856e0
RarCeth <rarceth@gmail.com>
2016-11-30 16:50:26 +0800

    Chunk

---
+++ Dragon Lands MK-3/Assets/Characters & AI/Dragons/Dragon_Main/Dragon_Main_0101_Script.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dragon_Main_0101_Script : MonoBehaviour {
+
+	public static GameObject mainDragon;
+
+	Player_Script player;
+
+	Animator anim;
+
+	Vector3 lastKnownGroundPoint;
+
+	bool takingOff, landing;
+
+	public bool onGround, canMove, isFlying;
+
+	public bool attacking;
+	int attackIndex;
+
+	public Transform rcPos;
+
+	public float groundDistance, turnSpeed, ascentSpeed;
+
+	public bool possessed;
+
+	void Awake () {
+		mainDragon = this.gameObject;
+		anim = GetComponent<Animator> ();
+		canMove = true;
+	}
+	// Use this for initialization
+	void Start () {
+		player = Player_Script.player.GetComponent<Player_Script> ();
+		lastKnownGroundPoint = transform.position;
+		attackIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		GroundDetection ();
+		DragonInput ();
+		DragonMovement ();
+		UpdateAnimationVariables ();
+	}
+
+	void GroundDetection () {
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity)) {
+			if (Vector3.Distance (transform.position, hit.point) < groundDistance) {
+				onGround = true;
+			} else {
+				onGround = false;
+				if (!isFlying && !takingOff && !landing) {
+					StartCoroutine (TakeOff ());
+				}
+			}
+			lastKnownGroundPoint = hit.point;
+		} else {
+			onGround = false;
+			transform.position += Vector3.up * 10 * Time.deltaTime;
+		}
+	}
+
+	void DragonInput () {
+		
+		if (possessed) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				if (isFlying) {
+					if (!takingOff && !landing) {
+						StartCoroutine (Land ());
+					}
+
+				} else {
+					if (!takingOff && !landing) {
+						StartCoroutine (TakeOff ());
+					}
+
+				}
+			}
+
+			if (Input.GetKeyDown (KeyCode.Mouse0)) {
+				if (!attacking && !takingOff && !landing) {
+					StartCoroutine (DragonAttack (0));
+				}
+
+			}
+
+			if (Input.GetKeyDown (KeyCode.Mouse1)) {
+				if (!attacking && !takingOff && !landing) {
+					StartCoroutine (DragonAttack (1));
+				}
+
+			}
+		}
+	}
+
+	void DragonMovement () {
+		if (canMove && possessed) {
+			anim.SetFloat ("Vertical", Input.GetAxis("DragonForward"));
+
+			if (isFlying) { 
+
+				Quaternion keyRot = Quaternion.Euler (new Vector3 (0f, turnSpeed * Input.GetAxis ("DragonHorizontal"), 0) * Time.deltaTime);
+				transform.rotation = transform.rotation * keyRot;
+
+				transform.position += new Vector3(0,Input.GetAxis ("DragonVertical") * ascentSpeed * Time.deltaTime,0);
+			} else {
+				if (Input.GetAxis("DragonForward") > 0) {
+					Quaternion keyRot = Quaternion.Euler (new Vector3 (0f, turnSpeed * Input.GetAxis ("DragonHorizontal"), 0) * Time.deltaTime);
+					transform.rotation = transform.rotation * keyRot;
+				}
+			}
+		}
+	}
+
+	IEnumerator DragonAttack (int attackType) {
+		attacking = true;
+		if (isFlying) {
+			attackIndex = Random.Range (0, 3);
+		} else {
+			attackIndex = Random.Range (0, 6);
+		}
+		anim.SetInteger ("attackIndex", attackIndex);
+		if (attackType == 0) {
+			anim.SetTrigger ("Attack");
+		} else {
+			anim.SetTrigger ("Attack_2");
+		}
+
+
+		yield break;
+	}
+
+	void UpdateAnimationVariables () {
+		anim.SetBool ("onGround", onGround);
+		anim.SetBool ("Flying", isFlying);
+	}
+
+	IEnumerator TakeOff () {
+		takingOff = true;
+		anim.SetTrigger ("Fly");
+		while (onGround){
+			transform.position += Vector3.up * Time.deltaTime;
+			yield return null;
+		}
+		isFlying = true;
+		takingOff = false;
+		yield break;
+	}
+
+	IEnumerator Land () {
+		landing = true;
+		if (onGround) {
+			anim.SetTrigger ("Land");
+			transform.position = lastKnownGroundPoint;
+		} else {
+			anim.SetTrigger ("Dive");
+			while (!onGround) {
+				yield return null;
+			}
+			anim.SetTrigger ("Land");
+			transform.position = lastKnownGroundPoint;
+		}
+		isFlying = false;
+		landing = false;
+		yield break;
+	}
+
+	IEnumerator GoToPlayer () {
+		transform.position = player.transform.position - player.transform.forward * 5;
+		yield break;
+	}
+
+
+
+
+}
---
+++ Dragon Lands MK-3/Assets/Characters & AI/Dragons/Dragon_Main/Dragon_Red_Script.cs
@@ -0,0 +1,185 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dragon_Red_Script : MonoBehaviour {
+
+	public static GameObject redDragon;
+
+	private Player_Script playerScript;
+
+	private Animator anim;
+
+	public bool isFlying;
+
+	public int currentHealth, maxHealth;
+
+	public bool dead;
+
+	public bool onGround;
+
+	public float turnSpeed, ascentSpeed;
+
+	bool takingOff, landing;
+
+	bool canMove = true;
+
+	public Transform mountTransform, dismountTransform, raycastTransform;
+
+	void Awake () {
+		redDragon = this.gameObject;
+		anim = GetComponent<Animator> ();
+	}
+	// Use this for initialization
+	void Start () {
+		playerScript = Player_Script.player.GetComponent<Player_Script> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckGround ();
+		if (isFlying && onGround) {
+			anim.SetTrigger ("Land");
+			isFlying = false;
+		}
+		anim.SetBool ("onGround", onGround);
+		anim.SetBool ("Flying", isFlying);
+
+		DragonMovement ();
+		if (Input.GetKeyDown (KeyCode.Keypad0)) {
+			ToggleFlightState ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.Comma)) {
+			Mount (Player_Script.player);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Period)) {
+			Dismount (Player_Script.player);
+		}
+	}
+
+	private void DragonMovement () {
+		if (!canMove) {
+			return;
+		}
+
+		anim.SetFloat ("Vertical", Input.GetAxis("DragonForward"));
+
+
+		if (isFlying) { 
+			
+			Quaternion keyRot = Quaternion.Euler (new Vector3 (0f, turnSpeed * Input.GetAxis ("DragonHorizontal"), 0) * Time.deltaTime);
+			transform.rotation = transform.rotation * keyRot;
+
+			transform.position += new Vector3(0,Input.GetAxis ("DragonVertical") * ascentSpeed * Time.deltaTime,0);
+		} else {
+			if (Input.GetAxis("DragonForward") > 0) {
+				Quaternion keyRot = Quaternion.Euler (new Vector3 (0f, turnSpeed * Input.GetAxis ("DragonHorizontal"), 0) * Time.deltaTime);
+				transform.rotation = transform.rotation * keyRot;
+			}
+		}
+
+	}
+
+	private void ToggleFlightState () {
+		isFlying = !isFlying;
+		if (isFlying) {
+			TakeOff ();
+		} else {
+			StartLand ();
+		}
+	}
+
+	private void TakeOff () {
+		anim.SetTrigger ("Fly");
+		isFlying = true;
+		StartCoroutine (TOIE());
+	}
+
+	private IEnumerator TOIE () {
+		takingOff = true;
+		for (int i = 0; i < 100; i++) {
+			transform.position += Vector3.up * Time.deltaTime;
+			yield return new WaitForSeconds(0.01f);
+		}
+		takingOff = false;
+		yield break;
+	}
+
+	private void StartLand () {
+		if (onGround) {
+			anim.SetTrigger ("Land");
+			isFlying = false;
+		} else {
+			anim.SetTrigger ("Dive");
+		}
+	}
+
+	private void Attack () {
+		anim.SetTrigger ("Attack");
+	}
+
+	private void GetHit () {
+		anim.SetTrigger ("Hit");
+	}
+
+	private void CheckHealth () {
+		if (currentHealth <= 0){
+			Kill ();
+		}
+	}
+
+	private Vector3 CheckGround () {
+		RaycastHit hit;
+		if (Physics.Raycast (raycastTransform.position, Vector3.down, out hit, 2.5f)) {
+			onGround = true;
+			return hit.point;
+		} else if (Physics.Raycast(raycastTransform.position, Vector3.down, out hit, Mathf.Infinity)){
+			onGround = false;
+			return hit.point;
+		} else {
+			if (!takingOff) {
+				anim.SetTrigger ("Land");
+				transform.position += Vector3.up * 1 * Time.deltaTime;
+				print ("not above ground at all");
+				return Vector3.zero;
+			} else {
+				print ("not above ground at all");
+				return Vector3.zero;
+			}
+
+
+		}
+	}
+
+	public void Kill () {
+		anim.SetTrigger ("Death");
+		dead = true;
+	}
+
+	public void Damage (int amount) {
+		currentHealth -= amount;
+		GetHit ();
+	}
+
+	public void Heal (int amount) {
+		currentHealth += amount;
+		currentHealth = Mathf.Clamp (currentHealth, 0, 100);
+	}
+
+	public void Mount (GameObject player) {
+		player.transform.position = mountTransform.position;
+		player.transform.rotation = mountTransform.rotation;
+		playerScript.PlayerMount ();
+
+		player.transform.parent = this.gameObject.transform;
+	}
+
+	public void Dismount (GameObject player) {
+		player.transform.position = dismountTransform.position;
+		player.transform.rotation = dismountTransform.rotation;
+		playerScript.PlayerDismount ();
+
+		player.transform.parent = null;
+	}
+}
