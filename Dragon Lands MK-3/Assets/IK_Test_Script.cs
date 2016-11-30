fb4d9682e5e75f55e867afc57ce8b15d648feb54
RarCeth <rarceth@gmail.com>
2016-11-30 18:17:28 +0800

    Chunk

---
+++ Dragon Lands MK-3/Assets/IK_Test_Script.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IK_Test_Script : MonoBehaviour {
+
+	Player_Script ps;
+	// Use this for initialization
+	void Start () {
+		ps = Player_Script.player.GetComponent<Player_Script> ();
+	}
+	
+	void Interact () {
+		ps.ikActive = true;
+		ps.ikLookObj = this.gameObject.transform;
+		ps.ikRightHandObj = transform.GetChild (0);
+	}
+}
