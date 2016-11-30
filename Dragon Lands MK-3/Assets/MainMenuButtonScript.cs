d2ec7d8e31b37402309ebb1066f64df24745e518
RarCeth <rarceth@gmail.com>
2016-11-30 18:20:14 +0800

    Chunk

---
+++ Dragon Lands MK-3/Assets/MainMenuButtonScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuButtonScript : MonoBehaviour {
+	public bool isNewGame, isContinue, isQuit;	
+
+	void Awake () {
+		if (isNewGame) {
+			GetComponent<Button> ().onClick.AddListener (() => NewGame ());
+		} else if (isContinue) {
+			GetComponent<Button> ().onClick.AddListener (() => Continue ());
+			if (PlayerPrefs.GetInt("HasSaveGame",0) == 0) {
+				GetComponent<Button> ().interactable = false;
+			}
+		} else if (isQuit) {
+			GetComponent<Button> ().onClick.AddListener (() => Quit ());
+		}
+	}
+
+	void OnEnable () {
+		if (isContinue) {
+			if (PlayerPrefs.GetInt("HasSaveGame",0) == 0) {
+				GetComponent<Button> ().interactable = false;
+			}
+		}
+
+	}
+
+	void NewGame () {
+		PlayerPrefs.SetInt ("HasSaveGame", 1);
+		print ("loading new game screen");
+		SceneManager.LoadScene ("Character_Creation_Scene");
+	}
+
+	void Continue () {
+		print ("loading previous game");
+		SceneManager.LoadScene ("Scene 01");
+	}
+
+	void Quit () {
+		print ("quit");
+		Application.Quit();
+	}
+}
